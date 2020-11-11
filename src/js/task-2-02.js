f9f48b7378d7c76768636711c539f84bffb74c56
Vitalii Chervonyi <vognuk@gmail.com>
2020-11-11 21:34:13 +0200

    3 - 3

--- src/js/task-2-02.js
+++ src/js/task-2-02.js
@@ -1,1 +1,21 @@
 console.log("Task-2-02");
+
+// Задание 2
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, 
+// а принимала всего два параметра allUsers и userName и возвращала промис.
+
+import users from "./users.js";
+
+const toggleUserState = (allUsers, userName) => {
+  return Promise.resolve(
+    allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    ),
+  );
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
